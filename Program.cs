d8d9ed25b89a40ad7de18d47fdaec43297d99c59
viktor <viktormanuel.cardenas@marquette.edu>
2024-04-18 15:10:13 -0500

    say hello in method

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿namespace githubinclass;
+﻿using System.Transactions;
+
+namespace githubinclass;
 
 class Program
 {
     static void Main(string[] args)
     {
         
+    }   
+    static void SayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    static void SayBye()
+    {
+        Console.WriteLine("Bye");
     }
 }
